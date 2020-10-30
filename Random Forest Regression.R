74a02c0b4702f144307031b589b0cb4444ccc604
dshah98 <dhairyashah429.ds@gmail.com>
2020-10-30 08:47:08 +0530

    Use Position_Salaries.csv File.

---
+++ Random Forest Regression.R
@@ -0,0 +1,47 @@
+
+# ---------------------------------------------------- Importing Data ------------------------------------------ #
+
+dataset = read.csv("Position_Salaries.csv")
+
+# Selecting particular columns
+dataset = dataset[2:3]
+
+# Level is the independent variable.
+# Salary is the dependent variable.
+
+# ---------------------------------- Fitting Support Vector Regression to the dataset -------------------------- #
+
+install.packages("randomForest")
+library("randomForest")
+set.seed(123)
+reg = randomForest(x = dataset[1], y = dataset$Salary, ntree = 10)
+summary(reg)
+
+# -------------------------------------------- Predictiing a new result  --------------------------------------- #
+
+y_pred = predict(reg, data.frame(Level = 6.5))
+y_pred
+
+# ------------------ Visualising the DTR Model results (for higher resolution and smoother curve) -------------- #
+
+library(ggplot2)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(reg, newdata = data.frame(Level = x_grid))), colour = 'blue') +
+  ggtitle('Truth or Bluff (Random Tree Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Increasing number of steps.
+
+set.seed(123)
+reg1 = randomForest(x = dataset[1], y = dataset$Salary, ntree = 100)
+
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary), colour = 'red') +
+  geom_line(aes(x = x_grid, y = predict(reg1, newdata = data.frame(Level = x_grid))), colour = 'blue') +
+  ggtitle('Truth or Bluff (Random Tree Regression)') +
+  xlab('Level') +
+  ylab('Salary')
